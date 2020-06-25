0a10e9f9888001e601899bc55a6e837b941fe660
Kuzeko @ CLAAUDIA <matteo@cs.aau.dk>
2020-06-25 16:54:43 +0200

    updated dbpedia download

--- DBpedia/download-dbpedia.sh
+++ DBpedia/download-dbpedia.sh
@@ -17,13 +17,16 @@ then
     echo "Downloading files..."
     rm -v ${DATA_DIR}/*.* || true
     while read -r line; do
+        [ -z "$line" ] && continue
         [[ "$line" =~ ^#.*$ ]] && continue
         wget -P ${DATA_DIR}/ $line
         bzip2 -dk ${DATA_DIR}/${line##*/}
         filename=$(basename -- "${DATA_DIR}/${line##*/}")
         filename="${filename%.*}"
-        iconv -f utf-8 -t ascii//TRANSLIT -c "${DATA_DIR}/${filename}" |  grep -v 'xn--b1aew'  > ${NEO4J_IMPORT}/${filename}
-        rm -v "${DATA_DIR}/${filename}"
+        #iconv -f utf-8 -t ascii//TRANSLIT -c "${DATA_DIR}/${filename}" |  grep -v 'xn--b1aew'  > ${NEO4J_IMPORT}/${filename}
+        iconv -f utf-8 -t ascii -c "${DATA_DIR}/${filename}" | grep -E '^<(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[A-Za-z0-9\+&@#/%?=~_|]>\W<' |   grep -v 'xn--b1aew' > ${NEO4J_IMPORT}/clean-${filename}
+
+        rm -v "${DATA_DIR}/${line##*/}"
     done < $1
 
     chmod -R 777 ${NEO4J_IMPORT}
